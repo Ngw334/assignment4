9f077a2c2b36abc5296503e9b69e18a4c23815cd
Ngw334 <82573863+Ngw334@users.noreply.github.com>
2021-05-18 20:54:52 +0530

    Add files via upload

---
+++ lectureQ1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int isEven(int in1)
+  {
+    for(int i=1; i < in1; i++)
+    {
+      if(i % 2==0)
+      {
+        cout<< i << endl;
+        continue;
+      }
+    }
+  }
+
+int main()
+{
+  int in1;
+  cout<< "Give value n to print all the even no. between 1 and n : ";
+  cin>> in1;
+  cout<< endl;
+  return isEven(in1);
+}
+
+
+
+
+
